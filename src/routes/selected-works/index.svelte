42b84e209a8102e594120eca354dd19063495193
Luca Claessens <lucaclae@gmail.com>
2020-11-30 15:13:20 +0100

    Feature/preview tests (#22)
    * added github actions
    
    * remove 'deploy' scripts from package
    
    * Removed empty qualifications row
    
    * added opengraph meta tags
    
    * added more meta config
    
    Co-authored-by: LookALuca <luca@lookaluca.com>

--- src/routes/selected-works/index.svelte
+++ src/routes/selected-works/index.svelte
@@ -10,12 +10,20 @@
 
 <script>
     import IndexLayout from "./../../components/index/IndexLayout.svelte";
+    import { page_host } from "./../core/config";
 
     export let entries;
 </script>
 
 <svelte:head>
     <title>Luca his archive of selected works</title>
+    <meta property="og:title" content="Luca his archive of selected works" />
+    <meta
+        property="og:description"
+        content="Selected works that contain some interesting content." />
+    <meta property="og:image" content="{page_host}/logo-512.png" />
+    <meta property="og:url" content="{page_host}/selected_works" />
+    <meta name="twitter:card" content="summary_large_image" />
     <meta
         name="Description"
         content="Selected works that contain some interesting content." />
