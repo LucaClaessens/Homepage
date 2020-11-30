1ceb6020c91584e6e8819b483948935db91ff41f
LookALuca <luca@lookaluca.com>
2020-11-30 15:17:56 +0100

    Squashed commit of the following:
    commit d8ff3d3ff8ca423dfa34b43dc9ba3e54a5290d06
    Author: LookALuca <luca@lookaluca.com>
    Date:   Mon Nov 30 15:15:48 2020 +0100
    
        fix: some paths
    
    commit bb12a9d911b9868b6c02fff0adfd38f67f32af4e
    Author: LookALuca <luca@lookaluca.com>
    Date:   Mon Nov 30 15:10:30 2020 +0100
    
        added more meta config
    
    commit 6fa7a92bc4f7031f28b4c883226e4ebd256d7dc8
    Merge: 0df1a55 2b9fad8
    Author: LookALuca <luca@lookaluca.com>
    Date:   Mon Nov 30 14:54:52 2020 +0100
    
        Merge branch 'feature/preview-tests' of https://github.com/LucaClaessens/Homepage into feature/preview-tests
    
    commit 0df1a551926298db6a1c3ae23149d8136c436511
    Author: LookALuca <luca@lookaluca.com>
    Date:   Mon Nov 30 14:54:41 2020 +0100
    
        added opengraph meta tags
    
    commit 2b9fad8795e729a0b0500e10916184f58cd142ca
    Merge: 119c155 7b23b76
    Author: Luca Claessens <lucaclae@gmail.com>
    Date:   Mon Nov 30 14:12:46 2020 +0100
    
        Merge branch 'preview' into feature/preview-tests
    
    commit 119c15582232403fd4491e69e9c9b5907d06b5ce
    Author: LookALuca <luca@lookaluca.com>
    Date:   Mon Nov 30 13:56:22 2020 +0100
    
        Removed empty qualifications row
    
    commit 650f2c1f087d98294d6c7dfeb11c57308f2b708d
    Author: LookALuca <luca@lookaluca.com>
    Date:   Mon Nov 30 13:55:46 2020 +0100
    
        remove 'deploy' scripts from package
    
    commit 3e6f8b17a9997f8878af09bd792eb8f6968bb755
    Author: LookALuca <luca@lookaluca.com>
    Date:   Mon Nov 30 13:52:06 2020 +0100
    
        added github actions

--- src/core/config.js
+++ src/core/config.js
@@ -1,1 +1,2 @@
-export const page_host = 'https://www.lucaclaessens.com';
+// export const page_host = 'https://www.lucaclaessens.com';
+export const page_host = 'https://lucaclaessens-32287--preview-y8sks951.web.app';
